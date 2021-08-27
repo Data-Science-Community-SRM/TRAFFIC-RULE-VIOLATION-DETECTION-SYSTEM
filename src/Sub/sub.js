0a55af330522501739159db802e9d798c48d5995
goviv <gv8464@srmist.edu.in>
2021-08-28 05:13:58 +0530

    minor changes

--- src/Sub/sub.js
+++ src/Sub/sub.js
@@ -8,9 +8,15 @@ const Sub = () => {
 <div ><div class="heading-title"id="project">Traffic Violation</div>
     
     <div class="traffic">In 2020, there were around 151 thousand deaths due to road accidents in India
-     and most of them are due to Signal Violation and No helmet. So we at Data Science Community
-     to help and reduce these deaths, have created photo based recognision system which help individuals
-     to find the type of people who drive without no helmets.
+     and most of them are due to Signal Violation and No helmet. In 2017 around 98 riders died everyday 
+     as they were not wearing a helmet.
+     <br /><br />
+     India is one of the largest motorcyle market in the world. It is also the second highest poulation 
+     in the world. For a major city like Bangalore or Mumbai it is really tough to keep a tab on people who violate
+     rules. So we at Data Science Community to help and reduce these deaths, have created photo based 
+     recognision system which help individuals to report people who drive without helmets.
+
+     
     </div>
     </div>
         <div class="wrapper">
